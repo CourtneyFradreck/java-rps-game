c548e221338046f9f2379a01f963e70c2e375340
CourtneyFradreck <courtaga@gmail.com>
2024-03-17 00:02:27 +0200

    imported everything from the java.util API

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
+import java.util.*;
+
 public class Main {
+    public static void main(String[]args){
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Hello Player, Do you want to play a game? (y/n)");
+        String yesOrNo = myObj.nextLine();
+        System.out.println("You answer was " +yesOrNo);
+    }
 }
